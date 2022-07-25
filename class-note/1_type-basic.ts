81cd83a7576888993d4c384c897d2d9ae1fa675c
sujungYu <su_moment@naver.com>
2022-07-25 17:15:53 +0900

    section 3 to 4

---
+++ class-note/1_type-basic.ts
@@ -0,0 +1,30 @@
+// 기본 타입
+
+// JS 문자열 선언
+// let str = 'hello';
+
+// TS 문자열 선언
+let str: string = 'hello';
+// TS 숫자
+let num: number = 10;
+// TS 배열
+let arr: Array<number> = [1,2,3]; //arr 변수는 배열 타임이고 배열안에 숫자만들어갈 수 있다.
+let heroes: Array<string> = ['Capt', 'Thor', 'Hulk'];
+let items: number[] = [1,2,3];
+
+// TS 튜플 - 특정 인덱스의 타입까지 정의하는 배열
+let address: [string, number] = ['gangnam', 100];
+
+// TS 객체
+let obj: object = {};
+// let person: object = {
+//     name: 'capt',
+//     age: 100
+// };
+let person: {name: string, age: number} = {
+    name: 'thor',
+    age: 1000
+}
+
+// TS 진위값
+let show: boolean = true;
--- quiz/1_todo/src/index.ts
+++ quiz/1_todo/src/index.ts
@@ -1,7 +1,15 @@
-let todoItems;
+// type Todo = { id: number; title: string; done: boolean };
+interface Todo {
+  id: number;
+  title: string;
+  done: boolean;
+}
+
+// let todoItems: object[]; // 배열안에 object가 들어간다.
+let todoItems: Todo[];
 
 // api
-function fetchTodoItems() {
+function fetchTodoItems(): Todo[] {
   const todos = [
     { id: 1, title: '안녕', done: false },
     { id: 2, title: '타입', done: false },
@@ -11,43 +19,62 @@ function fetchTodoItems() {
 }
 
 // crud methods
-function fetchTodos() {
+function fetchTodos(): object[] {
   const todos = fetchTodoItems();
   return todos;
 }
 
-function addTodo(todo) {
+function addTodo(todo: Todo): void {
   todoItems.push(todo);
 }
 
-function deleteTodo(index) {
+function deleteTodo(index: number): void {
   todoItems.splice(index, 1);
 }
 
-function completeTodo(index, todo) {
+function completeTodo(index: number, todo: Todo): void {
   todo.done = true;
   todoItems.splice(index, 1, todo);
 }
 
 // business logic
-function logFirstTodo() {
+function logFirstTodo(): object {
   return todoItems[0];
 }
 
-function showCompleted() {
+function showCompleted(): object[] {
   return todoItems.filter(item => item.done);
+  // return todoItems.filter(function(item) {
+  //   if(item.done) {
+  //     return item;
+  //   }
+  // })
 }
 
 // TODO: 아래 함수의 내용을 채워보세요. 아래 함수는 `addTodo()` 함수를 이용하여 2개의 새 할 일을 추가하는 함수입니다.
-function addTwoTodoItems() {
+function addTwoTodoItems(): void {
   // addTodo() 함수를 두 번 호출하여 todoItems에 새 할 일이 2개 추가되어야 합니다.
+  const item1 = {
+    id: 4,
+    title: '아이템4',
+    done: false,
+  };
+  addTodo(item1);
+  addTodo({
+    id: 5,
+    title: '아이템5',
+    done: false,
+  });
 }
 
 // NOTE: 유틸 함수
-function log() {
+function log(): void {
   console.log(todoItems);
 }
 
 todoItems = fetchTodoItems();
 addTwoTodoItems();
 log();
+
+// 타이핑(typing): 타입이 정의되지 않은 코드에 타입을 입혀주는 행위
+// void: 반환값이 없다고 명시적으로 지정
